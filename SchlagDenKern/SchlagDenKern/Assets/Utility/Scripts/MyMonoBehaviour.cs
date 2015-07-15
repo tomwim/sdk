0ef92eab866e570e516c411ecef3d98befdfc4c9
tomwim <samo.miw@gmail.com>
2015-07-16 00:04:21 +0200

    Add utility scripts (MyMonoBehaviour and Singleton)

---
+++ SchlagDenKern/SchlagDenKern/Assets/Utility/Scripts/MyMonoBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMonoBehaviour : MonoBehaviour {
+	
+	// Use this for initialization
+	public virtual void StartMe () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	public virtual void UpdateMe () 
+	{
+		
+	}
+}
---
+++ SchlagDenKern/SchlagDenKern/Assets/Utility/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MyMonoBehaviour where T : MyMonoBehaviour {
+	protected static T instance;
+	
+	/**
+      Returns the instance of this singleton.
+   */
+	public static T Instance
+	{
+		get
+		{
+			if(instance == null)
+			{
+				instance = (T) FindObjectOfType(typeof(T));
+				
+				if (instance == null)
+				{
+					Debug.LogError("An instance of " + typeof(T) + 
+					               " is needed in the scene, but there is none.");
+				}
+			}
+			
+			return instance;
+		}
+	}
+}
