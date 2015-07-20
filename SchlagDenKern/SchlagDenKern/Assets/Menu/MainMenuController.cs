50c2df312a59fadc11cf12fd08c23c28399a95c9
tomwim <samo.miw@gmail.com>
2015-07-20 23:28:09 +0200

    Add first version of MainMenuController

---
+++ SchlagDenKern/SchlagDenKern/Assets/Menu/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : Singleton<MainMenuController> 
+{
+
+	public bool loadGame;
+	public GameLoadingController.Game game;
+
+	void Start ()
+	{
+
+	}
+
+}
