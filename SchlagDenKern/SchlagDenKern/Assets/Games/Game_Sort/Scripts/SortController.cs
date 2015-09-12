666dca4644134e58fe0074592c7b67e3ae10c61e
tomwim <samo.miw@gmail.com>
2015-09-13 00:25:29 +0200

    Add basis for Sort game

---
+++ SchlagDenKern/SchlagDenKern/Assets/Games/Game_Sort/Scripts/SortController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortController : GameController
+{
+
+	/*
+	 * Get all sorting sets 
+	 */
+	private void Init ()
+	{
+		Guessables<SortingGuessable> guessables = XMLHandler.GetGuessableList<SortingGuessable>(GameLoadingController.Game.SORT);
+
+		if(guessables.ordered)
+		{
+			guessables.guessablesList = XMLHandler.SortByOrder(guessables.guessablesList);
+		}
+		else 
+		{
+			guessables.guessablesList = XMLHandler.Shuffle(guessables.guessablesList);
+		}
+
+
+	}
+	
+}
