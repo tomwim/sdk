a97e0979a1b0fdf4f7eababcb25ed3694ceb2c02
Tom Wim <TomWim@Toms-MacBook-Pro.local>
2015-09-18 10:51:15 +0200

    Add overriden Methods (Start, Next, Previous)

--- SchlagDenKern/SchlagDenKern/Assets/Games/Game_Sort/Scripts/SortController.cs
+++ SchlagDenKern/SchlagDenKern/Assets/Games/Game_Sort/Scripts/SortController.cs
@@ -1,8 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SortController : GameController
 {
+
+	private int currentSetIndex;
+	private List<SortingSet> sortingSets; 
+
+	public override void StartMe ()
+	{
+		base.StartMe ();
+		currentSetIndex = 0;
+		Init ();
+	}
+
+	public override void Solve ()
+	{
+
+	}
+
+	public override void Next ()
+	{
+		if (currentSetIndex < sortingSets.Count) 
+		{
+			currentSetIndex++;
+	
+		}
+	}
+
+	public override void Previous ()
+	{
+		if (currentSetIndex > 0) 
+		{
+			currentSetIndex--;
+		}
+	}
 
 	/*
 	 * Get all sorting sets 
