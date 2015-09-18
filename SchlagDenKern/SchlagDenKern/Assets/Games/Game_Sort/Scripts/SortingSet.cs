bfa0cc3a4f06e23c3d9477ac035f602ba8fa9ddd
Tom Wim <TomWim@Toms-MacBook-Pro.local>
2015-09-18 11:37:39 +0200

    Add SortingSet

---
+++ SchlagDenKern/SchlagDenKern/Assets/Games/Game_Sort/Scripts/SortingSet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SortingSet : MyMonoBehaviour 
+{
+	public Text topLabel;
+	public Text bottomLabel;
+
+	/* label at the top of the list */
+	public string TopLabelText 
+	{
+		get
+		{
+			return this.TopLabelText;
+		}
+		set 
+		{
+			this.TopLabelText = value;
+			topLabel.text = value;
+		}
+	}
+
+	/* label at the bottom of the list */
+	public string BottomLabelText 
+	{
+		get
+		{
+			return this.BottomLabelText;
+		}
+		set 
+		{
+			this.BottomLabelText = value;
+			bottomLabel.text = value;
+		}
+	}
+
+	private string[] elements;
+
+	public override void StartMe ()
+	{
+		base.StartMe ();
+		elements = new string[9];
+	}
+
+}
